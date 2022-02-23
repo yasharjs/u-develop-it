4e31afe05c08b6f379312c28f787ac65092e1406
Yashar Sarabi <77018659+yasharjs@users.noreply.github.com>
2022-02-23 18:34:08 -0500

    connected nodejs to mysql database

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,7 +8,23 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your mysql username,
+        user:'root',
+        // Your mysql password
+        password:'Cash1234',
+        database: 'election'
+    },
+    console.log("Connected to the election database.")
 
+);
+
+db.query(`SELECT * FROM candidates`,(err,rows)=>{
+    console.log(rows);
+})
 
 // Default response for any other request (Not Found)
 app.use((req,res)=>{
