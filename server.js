5017d9ba547f271336a4a3aca609197491865a7d
Yashar Sarabi <77018659+yasharjs@users.noreply.github.com>
2022-02-23 19:35:20 -0500

    build the database calss

--- server.js
+++ server.js
@@ -22,8 +22,36 @@ const db = mysql.createConnection(
 
 );
 
-db.query(`SELECT * FROM candidates`,(err,rows)=>{
-    console.log(rows);
+// db.query(`SELECT * FROM candidates`,(err,rows)=>{
+//     console.log(rows);
+// })
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id=1`,(err,row)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`,1,(err, result)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
+
+//Create a candidate
+const sql = `INSERT INTO candidates(id,first_name,last_name,industry_connected)
+Values (?,?,?,?)`;
+const params = [1,'Ronald','Firbank',1];
+
+db.query(sql,params,(err,result)=>{
+    if(err){
+        console.log(err); 
+    }
+    console.log(result);
 })
 
 // Default response for any other request (Not Found)
